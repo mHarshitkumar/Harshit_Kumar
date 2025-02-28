6d2d0a5c644d42277248f33f78dbc92993b62cdf
Harshit Kumar <harshit.kumar@thinkexam.com>
2025-02-28 14:55:47 +0530

    updated years

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -31,7 +31,7 @@ const About = () => {
       </div>
       <div className="about-achievements">
         <div className="about-achievement">
-            <h1>3.9+</h1>
+            <h1>3.11+</h1>
             <p>YEARS OF EXPERIENCE</p>
         </div>
         <hr />
